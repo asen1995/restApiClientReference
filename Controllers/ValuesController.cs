61db64c2713ba3da31b456a5f61f5fb602be1883
Asen Nikolaev <asen.nikolaev@nevexis.com>
2021-08-10 13:10:05 +0300

    testing webApi valuesController and README.md documentation

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace restApiClientReference.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "1", "2", "3", "4", "5", "6" };
         }
 
         // GET api/values/5
---
+++ README.md
@@ -0,0 +1,14 @@
+
+References from : 
+
+https://www.tutorialsteacher.com/webapi/create-web-api-project
+
+
+
+
+testing web api requests
+
+
+curl -k --request GET https://localhost:44380/Help
+
+curl -k --request GET https://localhost:44380/api/values
